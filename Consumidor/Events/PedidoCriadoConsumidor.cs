3785cc67814803223c83aa17e47976aed4097d5f
AndreTGama <andre_gama789@hotmail.com>
2023-12-02 20:35:57 -0300

    Adicionar arquivos de projeto.

---
+++ Consumidor/Events/PedidoCriadoConsumidor.cs
@@ -0,0 +1,15 @@
+﻿
+using Core.Entidades;
+using MassTransit;
+
+namespace Consumidor.Events
+{
+    public class PedidoCriadoConsumidor : IConsumer<Pedido>
+    {
+        public Task Consume(ConsumeContext<Pedido> context)
+        {
+            Console.WriteLine(context.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Produtor/Controllers/PedidoController.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Core.Entidades;
+
+namespace Produtor.Controllers
+{
+    [ApiController]
+    [Route("/pedido")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly IConfiguration _configuration;
+
+        public PedidoController(IBus bus, IConfiguration configuration)
+        {
+            _bus = bus;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var nomeFila = _configuration
+                .GetSection("MassTransit")["NomeFila"] ?? string.Empty;
+            var endponint = await _bus.GetSendEndpoint(
+                new Uri($"queue:{nomeFila}")
+            );
+
+            await endponint.Send(new Pedido(1,
+                new Usuario(1, "André Gama", "andre@email.com")));
+            return Ok();
+        }
+    }
+}
